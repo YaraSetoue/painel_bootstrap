3431b52f0218427130678363bfc28408e1ee122f
Yara Setoue <ysetoue@gmail.com>
2026-06-26 16:09:25 -0300

    Adicionado loading ao abrir uma aba

--- assets/js/painel.js
+++ assets/js/painel.js
@@ -1,5 +1,8 @@
 $(document).ready(() => {
     
+    const $containerListAba = $('#list-tab-aba');
+    const $containerPaneAba = $($containerListAba.data('target-pane'));
+    
     $("body").on('click', '#btnControlPainelLateral', function () {
 
         let $btn = $(this);
@@ -40,36 +43,71 @@ $(document).ready(() => {
         let $item = $(this);
         let $linkItem = $item.find('.nav-link');
 
-        let $containerList = $('#list-tab-aba');
-        let $containerPane = $($containerList.data('target-pane'));
+        let nomeAba = $linkItem.text();
+        let idAba = nomeAba.trim().toLowerCase().replace(/\s+/g, '');
 
-        let nameAba = $linkItem.text();
-        let idAba = nameAba.trim().toLowerCase().replace(/\s+/g, '');
+        criarAba(idAba, nomeAba);
+    });
+
+    // $(document).on('click', 'a', function(e){
+    //     e.preventDefault();
+    //     let $el = $(this);
+    //     let url = $el.attr('href');
+
+    //     if (
+    //         url === '#' ||
+    //         url.startsWith('http') ||
+    //         url.startsWith('mailto:') ||
+    //         url.startsWith('tel:')
+    //     ) {
+    //         return;
+    //     }
+
+    //     let nomeAba = $el.text();
+    //     let idAba = nomeAba.trim().toLowerCase().replace(/\s+/g, '');
+
+    //     $.get(url, function(){
+    //         criarAba(idAba, nomeAba);
+    //     });
+    // });
+
+    
+    function criarAba(idAba, nomeAba){
         
-        if($(`#list-${idAba}-item`).length == 0){
 
-            $containerList.append(`
+        if($(`#listAba-${idAba}-item`).length == 0){
+
+            $containerListAba.append(`
                 <div class="list-group-item">
-                    <a class="list-group-item-action" data-bs-toggle="list" href="#list-${idAba}-pane" id="list-${idAba}-item" role="tab" aria-controls="list-${idAba}-pane">
-                        ${nameAba}
+                    <a class="list-group-item-action" data-bs-toggle="list" href="#listAba-${idAba}-pane" id="listAba-${idAba}-item" role="tab" aria-controls="listAba-${idAba}-pane">
+                        ${nomeAba}
                     </a>
     
                     <button type="button" class="btn-close"></button>
                 </div>
             `);
     
-            $containerPane.append(`
-                <div class="tab-pane" id="list-${idAba}-pane" role="tabpanel">${nameAba}</div>
+            $containerPaneAba.append(`
+                <div class="tab-pane" id="listAba-${idAba}-pane" role="tabpanel">
+                    <div class="h-100 w-100 d-flex justify-content-center align-items-center flex-column js_loading">
+                        <span class="pb-2">Carregando ${nomeAba} </span>
+                        <div class="spinner-border" role="status">
+                        </div>
+                    </div>
+                </div>
             `);
-            
-            $linkItem.attr('href', `#list-${idAba}-item`);
         }
 
-        $containerList.find('.list-group-item-action.active').removeClass('active');
-        $($linkItem.attr('href')).toggleClass('active');
-        $('#list-pane-aba .tab-pane.active').removeClass('active');
-        $($($linkItem.attr('href')).attr('href')).toggleClass('active');
-    })
+        abrirAba(idAba);
+    }
+
+    function abrirAba(idAba){
+        $containerListAba.find('.list-group-item-action.active').removeClass('active');
+        $containerPaneAba.find('.tab-pane.show.active').removeClass('show active');
+
+        $(`#listAba-${idAba}-item`).addClass('active');
+        $(`#listAba-${idAba}-pane`).addClass('show active');
+    }
 
 
 });
